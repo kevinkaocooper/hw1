ada522c23fae45c487693cf5866ace9ff72d24f4
Cooper Kao <t.kao@cca.edu>
2018-09-14 22:16:44 -0700

    Update bubbles.js

--- bubbles.js
+++ bubbles.js
@@ -1,1 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.7.1/p5.min.js"></script>
+<style>
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
+</style>
+<meta charset="utf-8" />
+</head>
+<body>
+<script>
+
+var bubbles = [];
+
+function setup() {
+    createCanvas(1440, 800);
+
+    for (var i = 0; i < 30; i++) {
+        var bubble = {
+            x: random(width),
+            y: random(height),
+            radius: random(20, 150)
+        };
+        bubbles.push(bubble);
+    }
+}
+
+function draw() {
+    background(255);
+
+    for (var i = 0; i < bubbles.length; i++) {
+        var bubble = bubbles[i];
+
+        if (dist(mouseX, mouseY, bubble.x, bubble.y) < bubble.radius) {
+            if (mouseIsPressed) {
+                bubbles.splice(i, 1); // remove this bubble!
+            }
+            fill(240, 150, 98, 98);
+        } else {
+            fill(175, 199, 50, 29);
+            stroke(135, 150, 98, 98);
+        }
+
+        ellipse(bubble.x, bubble.y, bubble.radius*2);
+        bubble.x += random(5,-5);
+        bubble.y += random(-5,5);
+    }
+}
+
+</script>
+</body>
+</html>
 
